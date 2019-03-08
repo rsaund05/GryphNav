f49432715329b1b25a8b3d3d6997319774d798fc
rsaund05 <rsaund05@uoguelph.ca>
2019-03-08 13:02:40 -0500

    Progressing on MapKit stuff

--- GryphNav/ViewController.swift
+++ GryphNav/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    let regionRadius: CLLocationDistance = 210
+    let initialLocation = CLLocation(latitude: 43.53076529, longitude: -80.22899687)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,21 +27,48 @@ class ViewController: UIViewController {
         //Prefer larger title in navbar
         self.title = "GryphNav"
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        let optionsButton = UIBarButtonItem(title: "Options", style: .plain, target: self, action: #selector(optionsMenu))
+        let optionsButton = UIBarButtonItem(title: "Options", style: .plain, target: self, action: #selector(optionsButtonPressed(_:)))
         self.navigationItem.rightBarButtonItem = optionsButton
+        
+        //Setting up location stuff...
+        centerMapOnLocation(location: initialLocation)
+        requestLocationAccess()
+        mapView.delegate = self as MKMapViewDelegate
+        mapView?.showsUserLocation = true
+        //mapView?.addOverlay(<#T##overlay: MKOverlay##MKOverlay#>)
     }
-
+    
+    //Function for requesting location access
+    func requestLocationAccess() {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return
+            
+        case .denied, .restricted:
+            print("location access denied")
+            
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     @IBAction func optionsButtonPressed(_ sender: UIBarButtonItem) {
         print("Options pressed!")
+        performSegue(withIdentifier: "Options", sender: nil)
     }
     
-    @objc func optionsMenu(){
-        
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
-    
+//    func mapView(_ mapView: MKMapView, didAdd renderers: [MKOverlayRenderer]) {
+//        <#code#>
+//    }
 }
 
